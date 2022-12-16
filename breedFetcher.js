2206deb549fa7f2040ccfdc5c3ad43798d96b2d9
Joel Hagan <joel.hagan.8@gmail.com>
2022-12-16 15:14:59 -0700

    submission to test using api's for a url that provides information on cat breeds

---
+++ breedFetcher.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const fetchBreed = ((breedName, callback) => {
+  
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, (error, response, body) => {
+    
+    const data = JSON.parse(body);
+
+    if (error) {
+      callback(error);
+    } if (data[0]) {
+      callback(null, data[0].description);
+    } else {
+      callback('we could not find that specific breed name', null);
+    }
+
+  });
+});
